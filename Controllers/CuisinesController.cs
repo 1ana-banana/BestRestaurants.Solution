3479b15f12b63ee18e817b467184d0afa857e3a4
Anastasia Han <anastasia.han@yahoo.com>
2022-03-06 17:04:26 -0500

    Added basic Views and Controllers

---
+++ Controllers/CuisinesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BestRestaurants.Controllers
+{
+  
+}
---
+++ Controllers/RestaurantsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BestRestaurants.Controllers
+{
+  
+}
